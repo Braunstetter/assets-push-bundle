7dd87e80ce3db20b9c4b754005b14bef6fee86d2
Slowwie <michaelbrauner82@gmail.com>
2021-12-03 19:01:39 +0100

    Fix ExtensionInterface return issue

--- src/AssetsPushBundle.php
+++ src/AssetsPushBundle.php
@@ -3,11 +3,12 @@
 namespace Braunstetter\AssetsPushBundle;
 
 use Braunstetter\AssetsPushBundle\DependencyInjection\AssetsPushBundleExtension;
+use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class AssetsPushBundle extends Bundle
 {
-    public function getContainerExtension(): AssetsPushBundleExtension
+    public function getContainerExtension(): ?ExtensionInterface
     {
         if (null === $this->extension) {
             $this->extension = new AssetsPushBundleExtension();
